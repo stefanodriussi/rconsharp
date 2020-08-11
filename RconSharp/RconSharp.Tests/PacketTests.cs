93cc4647884427e0e46b93f7c3bdae942ab14dc7
Stefano Driussi <stedri@hotmail.it>
2020-08-11 11:42:03 +0200

    Remove license and Category attribute from tests

--- RconSharp/RconSharp.Tests/PacketTests.cs
+++ RconSharp/RconSharp.Tests/PacketTests.cs
@@ -1,37 +1,12 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Text;
 using Xunit;
 
-/*
-The MIT License (MIT)
-
-Copyright (c) 2014 Stefano Driussi
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 namespace RconSharp.Tests
 {
-	public class CoreTests
+    public class PacketTests
 	{
-		[Fact, Category("Packets")]
+		[Fact]
 		public void ConvertRconPacketToBytesShouldReturnValidRCONBuffer()
 		{
 			// Arrange
@@ -54,7 +29,8 @@ namespace RconSharp.Tests
 			Assert.Equal(content, Encoding.UTF8.GetString(buffer[12..^2]));
 			Assert.True(buffer[^1] == 0x00);
 		}
-		[Theory, Category("Packets")]
+
+		[Theory]
 		[InlineData("", PacketType.Auth, 15, 11)]
 		[InlineData(null, PacketType.Auth, 15, 11)]
 		public void ConvertEmptyContentRconPacketToBytesShouldReturnValidRCONBuffer(string content, PacketType packetType, int bufferLength, int packetLength)
@@ -76,9 +52,7 @@ namespace RconSharp.Tests
 			Assert.True(buffer[^1] == 0x00);
 		}
 
-
-
-		[Theory, Category("Packets")]
+		[Theory]
 		[InlineData(new byte[] { 17,0,0,0,1,0,0,0,3,0,0,0,112,97,115,115,119,111,114,100, 0,0 }, 17, 1, PacketType.Auth, "password")]
 		[InlineData(new byte[] { 10,0,0,0,1,0,0,0,3,0,0,0,0, 0,0 }, 10, 1, PacketType.Auth, "")]
         public void ConvertByteArrayShouldReturnValidRCONPacket(byte[] buffer, int size, int id, PacketType type, string body)
@@ -96,7 +70,7 @@ namespace RconSharp.Tests
 			Assert.Equal(type, packet.Type);
 		}
 
-		[Theory, Category("Packets")]
+		[Theory]
 		[InlineData(null, typeof(ArgumentNullException))]
 		[InlineData(new byte[] { 1 }, typeof(ArgumentException))]
 		[InlineData(new byte[] { 100, 0, 0, 0, 0 }, typeof(ArgumentOutOfRangeException))]
