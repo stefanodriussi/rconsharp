307aeff9328fac56097f4dc4a172464123cccd83
Stefano Driussi <stedri@hotmail.it>
2020-08-11 11:43:14 +0200

    Make ProgressiveId seeding thread safe

--- RconSharp/RconSharp/ProgressiveId.cs
+++ RconSharp/RconSharp/ProgressiveId.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace RconSharp
+﻿namespace RconSharp
 {
-	public static class ProgressiveId
+    /// <summary>
+    /// Progressive id counter starting from 0
+    /// </summary>
+    public static class ProgressiveId
 	{
 		private static object padlock = new object();
-		private static int counter = 0;
+		private static int counter = 1;
+		/// <summary>
+		/// Get next Id
+		/// </summary>
+		/// <returns>Next progressive Id</returns>
 		public static int Next()
 		{
 			lock (padlock)
@@ -15,7 +18,16 @@ namespace RconSharp
 				return counter++;
 			}
 		}
-
-		public static void Seed(int seed = 0) => counter = seed;
+		/// <summary>
+		/// Change the starting value for internal counter
+		/// </summary>
+		/// <param name="seed"></param>
+		public static void Seed(int seed = 0)
+		{
+			lock (padlock)
+			{
+				counter = seed;
+			}
+		}
 	}
 }
